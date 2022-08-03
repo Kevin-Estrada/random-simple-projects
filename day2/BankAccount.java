c897fb99b261c955957bb14435a05e333b070822
KevinEstrada12 <43767301+KevinEstrada12@users.noreply.github.com>
2022-08-03 00:51:56 -0500

    Added some edge case logic for some methods

--- day2/BankAccount.java
+++ day2/BankAccount.java
@@ -3,27 +3,34 @@ package day2;
 import java.util.Scanner;
 
 public class BankAccount {
-    int balance;
-    int previousTransaction;
-    String customerName;
-    String customerId;
+    private int balance;
+    private int previousTransaction;
+    private String customerName;
+    private static int Id = 0;
+    private int customerId = 0;
 
-    BankAccount(String cname, String cid) {
+    BankAccount(String cname) {
         customerName = cname;
-        customerId = cid;
+        Id += 1;
+        customerId = Id;
     }
 
     public void deposit(int amount) {
-        if (amount != 0) {
+
+        if (amount != 0 && amount > 0) {
             balance = balance + amount;
             previousTransaction = amount;
+        } else {
+            System.out.println("The amount " + amount + " is not a valid amount.");
         }
     }
 
     public void withdraw(int amount) {
-        if (amount != 0) {
+        if (amount != 0 && amount > 0) {
             balance = balance - amount;
             previousTransaction = -amount;
+        } else {
+            System.out.println("The amount " + amount + " is not a valid amount.");
         }
     }
 
@@ -37,59 +44,88 @@ public class BankAccount {
         }
     }
 
+    public void showId() {
+        System.out.println("Hi " + customerName + " your ID is: " + customerId);
+    }
+
+    public void showOptions() {
+        System.out.println("A. Check Balance");
+        System.out.println("B. Deposit");
+        System.out.println("C. Withdraw");
+        System.out.println("D. Previous Transaction");
+        System.out.println("E. Exit");
+    }
+
     public void showMenu() {
         char option = '\0';
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Welcome " + customerName);
         System.out.println("Your ID is " + customerId);
-        System.out.println("\n");
-        System.out.println("A. Check Balance");
-        System.out.println("B. Deposit");
-        System.out.println("C. Withdraw");
-        System.out.println("D. Previous Transaction");
-        System.out.println("E. Exit");
 
         do {
+            showOptions();
             System.out.println("===========================================================================");
             System.out.println("Enter an option");
             System.out.println("===========================================================================");
-            option = sc.next().charAt(0);
+            option = sc.next().toLowerCase().charAt(0);
             System.out.println("\n");
             switch (option) {
-                case 'A':
+                case 'a':
+                    System.out.println("Check Balance");
                     System.out.println("-----------------------------------");
                     System.out.println("Balance = " + balance);
                     System.out.println("-----------------------------------");
                     System.out.println("\n");
                     break;
 
-                case 'B':
-                    System.out.println("-----------------------------------");
-                    System.out.println("Enter an amount to deposit:");
-                    System.out.println("-----------------------------------");
-                    int amount = sc.nextInt();
-                    deposit(amount);
-                    System.out.println("\n");
+                case 'b':
+                    while (true) {
+                        System.out.println("Deposit");
+                        System.out.println("-----------------------------------");
+                        System.out.println("Enter an amount to deposit:");
+                        System.out.println("-----------------------------------");
+                        try {
+                            int amount = sc.nextInt();
+                            deposit(amount);
+                            System.out.println("\n");
+                            break;
+                        } catch (Exception e) {
+                            System.out.println("The value entered is not a number. Please enter a valid number.");
+                            sc.nextLine();
+                            System.out.println("\n");
+                        }
+                    }
                     break;
 
-                case 'C':
-                    System.out.println("-----------------------------------");
-                    System.out.println("Enter an amount to withdraw:");
-                    System.out.println("-----------------------------------");
-                    int amountTwo = sc.nextInt();
-                    withdraw(amountTwo);
-                    System.out.println("\n");
+                case 'c':
+                    while (true) {
+                        System.out.println("Withdraw");
+                        System.out.println("-----------------------------------");
+                        System.out.println("Enter an amount to withdraw:");
+                        System.out.println("-----------------------------------");
+                        try {
+                            int amountTwo = sc.nextInt();
+                            withdraw(amountTwo);
+                            System.out.println("\n");
+                            break;
+                        } catch (Exception e) {
+                            System.out.println("The value entered is not a number. Please enter a valid number.");
+                            sc.nextLine();
+                            System.out.println("\n");
+                        }
+                    }
                     break;
 
-                case 'D':
+                case 'd':
+                    System.out.println("Getting most recent transaction");
                     System.out.println("-----------------------------------");
                     getPreviousTransaction();
                     System.out.println("-----------------------------------");
                     System.out.println("\n");
                     break;
 
-                case 'E':
+                case 'e':
                     System.out.println("*******************************************************");
                     break;
 
@@ -98,7 +134,7 @@ public class BankAccount {
                     break;
             }
 
-        } while (option != 'E');
+        } while (option != 'e');
 
         System.out.println("Thank you for using our services!");
     }
--- day2/BankingApplication.java
+++ day2/BankingApplication.java
@@ -2,7 +2,12 @@ package day2;
 
 public class BankingApplication {
     public static void main(String[] args) {
-        BankAccount custOne = new BankAccount("Kevin", "000001");
+        BankAccount custOne = new BankAccount("Kevin");
+        BankAccount custTwo = new BankAccount("Ana");
+        BankAccount custThree = new BankAccount("Richi");
         custOne.showMenu();
+        // custOne.showId();
+        // custTwo.showId();
+        // custThree.showId();
     }
 }
